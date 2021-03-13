1c9825de04ec84d7df3d33169795fa290a64e2d5
Vu Tr <anvut20@gmail.com>
2021-03-14 04:51:27 +0700

    go into docker

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM php:7.4-fpm
+
+# Set working directory
+WORKDIR /var/www
+
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpng-dev \
+    libjpeg62-turbo-dev \
+    libfreetype6-dev \
+    libzip-dev \
+    locales \
+    zip \
+    jpegoptim optipng pngquant gifsicle \
+    vim \
+    unzip \
+    git \
+    curl
+
+# Clear cache
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install extensions
+RUN docker-php-ext-install pdo_mysql zip exif pcntl gd
+
+# Add user for laravel application
+#RUN groupadd -g 1000 www
+#RUN useradd -u 1000 -ms /bin/bash -g www www
+
+# Copy existing application directory contents
+COPY . /var/www
+RUN chown www-data:www-data /var/www -R
+RUN chmod 755 /var/www -R
+
+# Expose port 9000 and start php-fpm server
+EXPOSE 9000
+CMD ["php-fpm"]
--- init_db.sql
+++ init_db.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 26, 2020 at 07:12 AM
--- Server version: 10.4.12-MariaDB
--- PHP Version: 7.4.4
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2020 at 12:35 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -132,12 +132,22 @@ CREATE TABLE `book_user` (
 
 INSERT INTO `book_user` (`created_at`, `updated_at`, `rating`, `review`, `owned`, `book_id`, `user_id`) VALUES
 (NULL, NULL, 2, 'I don\'t like this book.', 1, 1, 1),
-(NULL, NULL, NULL, 'yeah this book sux tho', 0, 1, 3),
 (NULL, NULL, NULL, NULL, 1, 2, 1),
-(NULL, NULL, NULL, 'This book is legit, ngl i love this book', 1, 2, 3),
-(NULL, NULL, NULL, NULL, 1, 3, 3),
-(NULL, NULL, NULL, NULL, 1, 6, 3),
-(NULL, NULL, NULL, NULL, 0, 9, 3);
+(NULL, NULL, NULL, 'It\'s not bad', 1, 2, 4),
+(NULL, NULL, NULL, 'Interesting but not so much', 1, 3, 4),
+(NULL, NULL, NULL, NULL, 1, 9, 4),
+(NULL, NULL, NULL, NULL, 1, 10, 2),
+(NULL, NULL, NULL, NULL, 1, 10, 4),
+(NULL, NULL, NULL, 'Not as good as the following one. But the following one hasn\'t been published yet.', 1, 11, 2),
+(NULL, NULL, NULL, NULL, 1, 11, 4),
+(NULL, NULL, NULL, NULL, 1, 14, 4),
+(NULL, NULL, NULL, NULL, 1, 15, 2),
+(NULL, NULL, NULL, NULL, 1, 15, 4),
+(NULL, NULL, NULL, NULL, 1, 17, 2),
+(NULL, NULL, NULL, NULL, 1, 19, 2),
+(NULL, NULL, NULL, NULL, 1, 19, 4),
+(NULL, NULL, NULL, NULL, 1, 21, 2),
+(NULL, NULL, NULL, NULL, 1, 24, 4);
 
 -- --------------------------------------------------------
 
@@ -232,11 +242,17 @@ CREATE TABLE `oauth_access_tokens` (
 --
 
 INSERT INTO `oauth_access_tokens` (`id`, `user_id`, `client_id`, `name`, `scopes`, `revoked`, `created_at`, `updated_at`, `expires_at`) VALUES
+('03a409447e81aedf10c5bb4ba0e3292734b2e8f49102a6991cdfd47af68de40e61e22e2b4a03cde8', 2, 2, NULL, '[]', 0, '2020-07-03 03:33:01', '2020-07-03 03:33:01', '2021-07-03 10:33:01'),
 ('20e4e8d3453f874c742230a48a5d9d95e8d9e946f26d47e9a0db83ee419e56d4bf9729ae9ed59414', 3, 2, NULL, '[]', 0, '2020-06-24 10:17:49', '2020-06-24 10:17:49', '2021-06-24 17:17:49'),
+('2fe7a7750e26ef780cf491ee8b5ad3decb3208800077f9664a59a529e5a5933720f3b15c0e593c09', 1, 2, NULL, '[]', 0, '2020-07-02 22:42:28', '2020-07-02 22:42:28', '2021-07-03 05:42:28'),
 ('32f098f714efd482491cfccbcb83db2fdb4edda2b1679c8471cd836c7094b6602135b15bed9edb9d', 1, 2, NULL, '[]', 0, '2020-06-22 09:06:12', '2020-06-22 09:06:12', '2021-06-22 16:06:12'),
 ('7dc80e65f39475a7d94b67149e455af5f60fc32900f388065a0569ff287032d15a222c2218b2771c', 3, 2, NULL, '[]', 0, '2020-06-24 02:22:00', '2020-06-24 02:22:00', '2021-06-24 09:22:00'),
 ('7fd7eeeccd58dfdabecfde11d4040a952866546db7b490960cae6d4862ecd0a743e3af057bf20c06', 3, 2, NULL, '[]', 0, '2020-06-25 02:39:51', '2020-06-25 02:39:51', '2021-06-25 09:39:51'),
+('875f59a69eaa1aaa7b5cc463372e0d8a811edd5378438e39ad8496119c3c00ffdd4aa3b4f29341bc', 4, 2, NULL, '[]', 0, '2020-07-03 03:30:46', '2020-07-03 03:30:46', '2021-07-03 10:30:46'),
+('c6aca31510c65485a98206e4527b3fa4887e49c292915ae5c58aedb8080f3c492f8c02d8e2e4de0c', 2, 2, NULL, '[]', 0, '2020-07-02 22:42:04', '2020-07-02 22:42:04', '2021-07-03 05:42:04'),
 ('da426ef85c164512f44ef783c9498087125204c0999f3d977ce9839c1c852ed5cde8444585667a26', 3, 2, NULL, '[]', 0, '2020-06-24 01:49:32', '2020-06-24 01:49:32', '2021-06-24 08:49:32'),
+('e252741ad2725c4610f69d3069aa7c797a7050908b5aa7ee6da9666cfa0e781dfe62a96f4a502537', 1, 2, NULL, '[]', 0, '2020-07-02 22:44:03', '2020-07-02 22:44:03', '2021-07-03 05:44:03'),
+('e4be38eecf5ba51ce7d0476c52136d880078046aea580676489a548a64b185ecfd146b6391b9595f', 4, 2, NULL, '[]', 0, '2020-07-02 22:43:19', '2020-07-02 22:43:19', '2021-07-03 05:43:19'),
 ('ec87c32efc3ec9e2c35cbb39fe818803e4c0ad58a4dc1eeccbd0231f888f15da1555b932704c1d6c', 3, 2, NULL, '[]', 0, '2020-06-25 06:44:44', '2020-06-25 06:44:44', '2021-06-25 13:44:44'),
 ('f86c3e97458080892efc312c4338bd80c6a1dc9644fcbf474d5d830072989c8710bd6b2084997e1d', 3, 2, NULL, '[]', 0, '2020-06-24 10:41:15', '2020-06-24 10:41:15', '2021-06-24 17:41:15'),
 ('fb24e46dbbbd3bc300a530a1b98c73e10b4358517462d05041d51f560509f8a8e7b0a36ac9b586cc', 3, 2, NULL, '[]', 0, '2020-06-24 01:49:16', '2020-06-24 01:49:16', '2021-06-24 08:49:16');
@@ -322,13 +338,19 @@ CREATE TABLE `oauth_refresh_tokens` (
 --
 
 INSERT INTO `oauth_refresh_tokens` (`id`, `access_token_id`, `revoked`, `expires_at`) VALUES
+('09fda8b95333fe4326dc30db095485fd6325b0b2f0bc48bab88679fe860b4b53f9eb85fc406edd63', 'e4be38eecf5ba51ce7d0476c52136d880078046aea580676489a548a64b185ecfd146b6391b9595f', 0, '2021-07-03 05:43:19'),
 ('1599080a3ef16ac517127f483a03b26e833f38d9feeae77b81d823c6616456aba26496dcd2670e80', 'fb24e46dbbbd3bc300a530a1b98c73e10b4358517462d05041d51f560509f8a8e7b0a36ac9b586cc', 0, '2021-06-24 08:49:16'),
 ('18e2f65a06e7d08efd1d9081ff394fffec2d63949b72e469f3fe9637ced3366e48b9f6ff7f510102', '20e4e8d3453f874c742230a48a5d9d95e8d9e946f26d47e9a0db83ee419e56d4bf9729ae9ed59414', 0, '2021-06-24 17:17:49'),
 ('193d70062861d478d7a0d4dea3773e7d3f866a562994d2b71ca3428b982454c9e97410338e165edc', '7fd7eeeccd58dfdabecfde11d4040a952866546db7b490960cae6d4862ecd0a743e3af057bf20c06', 0, '2021-06-25 09:39:51'),
+('39dd1d5fb3a6f64bc982ea527d04872e89f1d5aab26be349941d7c6c67c99d7d2b866a6f7fc6515f', 'c6aca31510c65485a98206e4527b3fa4887e49c292915ae5c58aedb8080f3c492f8c02d8e2e4de0c', 0, '2021-07-03 05:42:04'),
 ('5d506f8ba370094016ae263cc0edf2bb9a184571f7984270a3197c923bf7c6b230c20c9a8b8d3cf9', '32f098f714efd482491cfccbcb83db2fdb4edda2b1679c8471cd836c7094b6602135b15bed9edb9d', 0, '2021-06-22 16:06:12'),
 ('763b12a165ec79d022ff751593a73a2bb7da3e7b0cb42e5431d287eea027bfc73f0eeb094c5eee1c', 'ec87c32efc3ec9e2c35cbb39fe818803e4c0ad58a4dc1eeccbd0231f888f15da1555b932704c1d6c', 0, '2021-06-25 13:44:44'),
 ('9543927661fe632c2ccd62624ed60c8442fb5299038b24b9e523f3e31b4040bbe299499b961908ba', 'da426ef85c164512f44ef783c9498087125204c0999f3d977ce9839c1c852ed5cde8444585667a26', 0, '2021-06-24 08:49:32'),
+('9605262b5b39033e2a47a11865e6aaa50528498fd00d523f6a4849cf1eae2bae16ae3f04259beb83', '2fe7a7750e26ef780cf491ee8b5ad3decb3208800077f9664a59a529e5a5933720f3b15c0e593c09', 0, '2021-07-03 05:42:28'),
+('b9fe8fe6f1bbf8893a0fab7332eea3a7e8260d8f642d405ed51fac2d65dcbc5d6a06513ce0032f40', 'e252741ad2725c4610f69d3069aa7c797a7050908b5aa7ee6da9666cfa0e781dfe62a96f4a502537', 0, '2021-07-03 05:44:03'),
+('bc3ac2056ca1909e2ec22190188b0bea7c315bcd147d42946ff3acf0f7426e3b8dd2d33d053e97ec', '03a409447e81aedf10c5bb4ba0e3292734b2e8f49102a6991cdfd47af68de40e61e22e2b4a03cde8', 0, '2021-07-03 10:33:01'),
 ('d5a5f4376a6049a001112298acf507a920f1662e89b87ffe31047c2dd586cdd9979e533051b871c9', 'f86c3e97458080892efc312c4338bd80c6a1dc9644fcbf474d5d830072989c8710bd6b2084997e1d', 0, '2021-06-24 17:41:15'),
+('e1ed38a556b2b35f6567a351cdf6ea80d7e1940ab77acd6ada3364e7f2ab10fa28a6eb86831e3c96', '875f59a69eaa1aaa7b5cc463372e0d8a811edd5378438e39ad8496119c3c00ffdd4aa3b4f29341bc', 0, '2021-07-03 10:30:46'),
 ('f8913960145421ba1ee6dd0da21e5b5bb882a373114a42be9b17e3f637ebfa7db2821930fc428700', '7dc80e65f39475a7d94b67149e455af5f60fc32900f388065a0569ff287032d15a222c2218b2771c', 0, '2021-06-24 09:22:00');
 
 -- --------------------------------------------------------
@@ -356,9 +378,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `avatar`, `provider`, `provider_id`) VALUES
-(1, 'Vu Anh Tran', 'avk13@gm.com', NULL, '$2y$10$9pIhtej4JasBBZZrA.zhguY6YkLBqbSlu1kqRB8HS9FRGz5sq9QPq', NULL, NULL, NULL, NULL, NULL, NULL),
+(1, 'Vu Anh Tran', 'avk13@gm.com', NULL, '$2y$10$xb/aMVD2UzCm5TMmsJvMcuHAtM1I7prMM1aqeQAxTUHRZTJxQECRi', NULL, NULL, NULL, NULL, NULL, NULL),
 (2, 'Ha Dang Luu', 'viole@gm.com', NULL, '$2y$10$xb/aMVD2UzCm5TMmsJvMcuHAtM1I7prMM1aqeQAxTUHRZTJxQECRi', NULL, NULL, NULL, NULL, NULL, NULL),
-(3, 'halu', 'halu@gm.com', NULL, '$2y$10$8A0BQTDqzNxBwZbg88vrhOku8uMjd4pPJ.Xci/DG0G6rr.QPf88Yu', NULL, '2020-06-24 01:49:16', '2020-06-24 01:49:16', NULL, NULL, NULL);
+(4, 'Nhi Phuong Le', 'ltpn@gm.com', NULL, '$2y$10$xb/aMVD2UzCm5TMmsJvMcuHAtM1I7prMM1aqeQAxTUHRZTJxQECRi', NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -486,7 +508,7 @@ ALTER TABLE `oauth_personal_access_clients`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
